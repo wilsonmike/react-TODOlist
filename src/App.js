5522f3dcfef831b2562ec8199eb1066f81bc70c7
mike <michaelwilsonsj@hotmail.com>
2020-11-03 18:27:09 -0500

    added completed todo functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 // import comp
-import Form from "./components/Form"; 
+import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
   const [inputText, setInputText] = useState("");
-  const [todos, setTodos] = useState([]);  
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
   return (
     <div className="App">
       <header>
-        <h3><i className="fas fa-bullseye"></i> task[ez]</h3>
+        <h3>
+          <i className="fas fa-bullseye"></i> task[ez]
+        </h3>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos = {setTodos} setInputText={setInputText}/>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
       <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
 
 export default App;
-
 
 //52:11
